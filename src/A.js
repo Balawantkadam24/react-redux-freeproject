b0cbabf01564b72081f19561b30d53ce1f4bc542
Balawantkadam24 <Balawantkadam24@gamil.com>
2022-02-20 21:42:43 +0530

    initial commit

---
+++ src/A.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function A(){
+return(
+   <div>
+       <h2>Welcome to world</h2>
+  </div>
+);
+
+}
+
+export default A;
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+//import Users from './Users'
+//import { Provider } from 'react-redux'
+//import store from '../src/redux/user/store'
+import A from './A';
+function App() {
+  
+  return (
+    <div className="App">
+     <h1>Welcome to react</h1>
+     <A />
+    </div>
+  );
+}
+
+export default App;
